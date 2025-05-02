f631a507423bac6445afa9a1feea12e56c3e5072
Sumukha <sumukha@zerozilla.com>
2025-05-02 23:19:34 +0530

    chore: update dependencies and add Tailwind CSS support
    - Added nativewind and tailwindcss to package.json
    - Updated react-native-reanimated and react-native-safe-area-context versions
    - Created globals.css for Tailwind CSS styles
    - Updated RootLayout to import globals.css
    - Modified Index component to use Tailwind CSS classes for styling
    - Added tailwind.config.js for Tailwind CSS configuration
    - Updated tsconfig.json to include nativewind-env.d.ts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,8 +8,11 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
+      className="bg-yellow-500"
     >
-      <Text>Edit app/index.tsx to edit this screen 123.</Text>
+      <Text className="bg-purple-200 text-white p-4 rounded-lg text-lg font-bold">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
