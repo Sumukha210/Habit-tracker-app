0316aa5fcbca1274ce91c816d8df97616b09e426
Sumukha <sumukha@zerozilla.com>
2025-05-03 00:34:31 +0530

    chore: update ESLint configuration to include additional ignores and improve rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: [
+      "dist/*",
+      "node_modules/*",
+      "ios/*",
+      "android/*",
+      "web-build/*",
+      "web-build-ssr/*",
+      "web-build-ssr-prod/*",
+      "web-build-prod/*",
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-vars": "off", // Turn off the base rule as it can report incorrect errors
+    },
   },
 ]);
